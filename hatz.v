cf465f1a040081fbff9784c44dca358b83d959c6
Alin-Alexandru Stoica <alstoica@ias.bitdefender.biz>
2021-01-10 20:25:43 +0200

    stuff

--- hatz.v
+++ hatz.v
@@ -27,7 +27,7 @@ Inductive AExp :=
   | mul: AExp -> AExp -> AExp
   | div: AExp -> AExp -> AExp
   | modulo: AExp -> AExp -> AExp
-  | arr_a_element: string -> defaultNat -> AExp.
+  | arr_a_element: string -> AExp -> AExp.
 
 Inductive BExp:=
   | true_b
@@ -43,13 +43,13 @@ Inductive BExp:=
   | gtthaneq: AExp -> AExp -> BExp
   | eq_b: AExp -> AExp -> BExp
   | neq_b: AExp -> AExp -> BExp
-  | arr_b_element: string -> defaultNat -> BExp.
+  | arr_b_element: string -> AExp -> BExp.
 
 Inductive SExp :=
   | var_s : string -> SExp
   | default_s: defaultStr -> SExp
   | str_concat: SExp -> SExp -> SExp
-  | arr_s_element: string -> defaultNat -> SExp.
+  | arr_s_element: string -> AExp -> SExp.
 
 Inductive Exp :=
   | struct_member : string -> string -> Exp
@@ -59,17 +59,17 @@ Inductive Stmt :=
   | assgn_n : string -> AExp -> Stmt
   | assgn_b : string -> BExp -> Stmt
   | assgn_s : string -> SExp -> Stmt
-  | decl_arr_n : string -> AExp -> Stmt
-  | decl_arr_b : string -> AExp -> Stmt
-  | decl_arr_s : string -> AExp -> Stmt
+  | assgn_arr_n : string -> AExp -> AExp -> Stmt
+  | assgn_arr_b : string -> AExp -> BExp -> Stmt
+  | assgn_arr_s : string -> AExp -> SExp -> Stmt
   | seq : Stmt -> Stmt -> Stmt
   | while : BExp -> Stmt -> Stmt
   | ifthen : BExp -> Stmt -> Stmt
   | ifelse : BExp -> Stmt -> Stmt -> Stmt
   | switch_case : AExp -> list Case -> Stmt
     with Case :=
-      | default : Stmt -> Case
-      | case : AExp -> Stmt -> Case.
+      | default_case : Stmt -> Case
+      | case__ : AExp -> Stmt -> Case.
 
 Inductive Func :=
   | def_n : string -> list Param -> Stmt -> Func
@@ -127,13 +127,13 @@ Notation "A ;; B" := (seq A B) (at level 87).
 Notation "'while_' ( B ) { S }" := (while B S) (at level 86).
 Notation "'if_' B { S }" := (ifthen B S) (at level 86).
 Notation "'if_' B { S0 } 'else_' { S1 }" := (ifelse B S0 S1) (at level 86).
-Notation "'default' { S }" := (default S) (at level 85).
-Notation "'case' ( A ) { S }" := (case A S) (at level 85).
+Notation "'default' { S }" := (default_case S) (at level 85).
+Notation "'case' ( A ) { S }" := (case__ A S) (at level 85).
 Notation "'switch' ( A ) { C0  C1  ..  Cn }" := (switch_case A (cons C0(cons C1 .. (cons Cn nil) ..))) (at level 86).
 
-Notation "'nat_' S [ A ]" := (decl_arr_n S A) (at level 80).
-Notation "'bool_' S [ A ]" := (decl_arr_b S A) (at level 80).
-Notation "'str_' S [ A ]" := (decl_arr_s S A) (at level 80).
+Notation "'nat_' S [ A ] ::= X" := (assgn_arr_n S A X) (at level 80).
+Notation "'bool_' S [ A ] ::= X" := (assgn_arr_b S A X) (at level 80).
+Notation "'str_' S [ A ] ::= X" := (assgn_arr_s S A X) (at level 80).
 
 Notation "'nat_p' X" := (param_n X) (at level 82).
 Notation "'bool_p' X" := (param_b X) (at level 82).
@@ -148,7 +148,7 @@ Notation "'str_m' X" := (member_s X) (at level 82).
 Notation "'struct' S { M0 ';;' M1 ';;' .. ';;' Mn ';;' }" := (def_struct S (cons M0(cons M1 .. (cons Mn nil) ..))) (at level 81).
 
 Definition test_stmt :=
-  nat_ "test" [100];;
+  nat_ "test" [100] ::= 1;;
   nat_ "hatz" ::= get_a "test"[69];;
   nat_ "test" ::= 1337;;
   bool_ "test1" ::= true;;
@@ -157,7 +157,7 @@ Definition test_stmt :=
 
   while_ ("test" >' 0) {
     nat_ "test" ::= "test" -' 1;;
-    str_ "test"[2077]
+    str_ "test"[2077] ::= "eigfhjao"
   } ;;
 
   switch ("test"){
@@ -173,14 +173,14 @@ Definition test_stmt :=
   }
 .
 
-Definition test_func :=
+(*Definition test_func :=
   nat_f "test_function" # nat_p "param0", bool_p "param1", str_p "param2" #
   {
     str_ "test" ::= "iagefh";;
     nat_ "thing" ::= 124
   }
 .
-(*
+
 Definition test_struct :=
   struct "test" 
   {
@@ -189,3 +189,143 @@ Definition test_struct :=
     str_m "test2" 
   }
 .*)
+
+Definition NEnv := string -> nat.
+Definition BEnv := string -> bool.
+Definition SEnv := string -> string.
+
+Definition NArrEnv := string -> nat -> nat.
+Definition BArrEnv := string -> nat -> bool.
+Definition SArrEnv := string -> nat -> string.
+
+Inductive Env :=
+  | env_pack : NEnv -> BEnv -> SEnv -> NArrEnv -> BArrEnv -> SArrEnv -> Env.
+
+Definition NUpdate (env : NEnv) (var : string) (value : nat) : NEnv :=
+  fun var' => if (eqb var' var)
+                then value
+                else (env var').
+
+Definition NArrUpdate (env : NArrEnv) (var : string) (index : nat) (value : nat) : NArrEnv :=
+  fun var' index' => if (andb (eqb var' var) (Nat.eqb index index'))
+                     then value
+                     else (env var' index').
+
+Definition BUpdate (env : BEnv) (var : string) (value : bool) : BEnv :=
+  fun var' => if (eqb var' var)
+                then value
+                else (env var').
+
+Definition BArrUpdate (env : BArrEnv) (var : string) (index : nat) (value : bool) : BArrEnv :=
+  fun var' index' => if (andb (eqb var' var) (Nat.eqb index index'))
+                     then value
+                     else (env var' index').
+
+Definition SUpdate (env : SEnv) (var : string) (value : string) : SEnv :=
+  fun var' => if (eqb var' var)
+                then value
+                else (env var').
+
+Definition SArrUpdate (env : SArrEnv) (var : string) (index : nat) (value : string) : SArrEnv :=
+  fun var' index' => if (andb (eqb var' var) (Nat.eqb index index'))
+                     then value
+                     else (env var' index').
+
+Definition env_test : NEnv :=
+  fun var => if (string_dec var "hatz") then 69 else 0.
+
+Compute (env_test "hatz").
+Compute (env_test "not_hatz").
+
+Definition senv_test : SEnv :=
+  fun var => if (string_dec var "hatz") then "gionule" else "".
+
+Compute (senv_test "hatz").
+Compute (senv_test "not_hatz").
+
+Definition benv_arr_test : BArrEnv :=
+  fun var idx => if (andb (eqb var "testing") (Nat.eqb idx 0)) then true else false.
+
+Compute (benv_arr_test "testing" 0).
+Compute ((BArrUpdate benv_arr_test "testing" 0 false) "testing" 0).
+
+Fixpoint AEval (a : AExp) (env : NEnv) (arrenv : NArrEnv) : nat :=
+  match a with
+    | var_a var => env var
+    | default_a def_ => match def_ with
+                          | error_nat => 0
+                          | natural n => n 
+                        end
+    | add a0 a1 => (AEval a0 env arrenv) + (AEval a1 env arrenv)
+    | diff a0 a1 => (AEval a0 env arrenv) - (AEval a1 env arrenv)
+    | mul a0 a1 => (AEval a0 env arrenv) * (AEval a1 env arrenv)
+    | div a0 a1 => Nat.div (AEval a0 env arrenv) (AEval a1 env arrenv)
+    | modulo a0 a1 => Nat.modulo (AEval a0 env arrenv) (AEval a1 env arrenv)
+    | arr_a_element arr idx => arrenv arr (AEval idx env arrenv)
+  end.
+  
+Fixpoint BEval (b : BExp) (benv : BEnv) (barrenv : BArrEnv) (nenv : NEnv) (narrenv : NArrEnv) : bool :=
+  match b with
+    | true_b => true
+    | false_b => false
+    | var_b var => benv var
+    | default_b def_ => match def_ with
+                          | error_bool => false
+                          | boolean bv => bv
+                        end
+    | not_b b0 => negb (BEval b0 benv barrenv nenv narrenv)
+    | and_b b0 b1 => andb (BEval b0 benv barrenv nenv narrenv) (BEval b1 benv barrenv nenv narrenv)
+    | or_b b0 b1 => orb (BEval b0 benv barrenv nenv narrenv) (BEval b1 benv barrenv nenv narrenv)
+    | lessthan a0 a1 => Nat.leb (AEval a0 nenv narrenv) (AEval a1 nenv narrenv)
+    | lessthaneq a0 a1 => orb (Nat.eqb (AEval a0 nenv narrenv) (AEval a1 nenv narrenv)) (Nat.leb (AEval a0 nenv narrenv) (AEval a1 nenv narrenv))
+    | gtthan a0 a1 => Nat.leb (AEval a1 nenv narrenv) (AEval a0 nenv narrenv)
+    | gtthaneq a0 a1 => orb (Nat.eqb (AEval a0 nenv narrenv) (AEval a1 nenv narrenv)) (Nat.leb (AEval a1 nenv narrenv) (AEval a0 nenv narrenv))
+    | eq_b a0 a1 => Nat.eqb (AEval a0 nenv narrenv) (AEval a1 nenv narrenv)
+    | neq_b a0 a1 => negb (Nat.eqb (AEval a0 nenv narrenv) (AEval a1 nenv narrenv))
+    | arr_b_element arr idx => barrenv arr (AEval idx nenv narrenv)
+  end.
+
+Fixpoint SEval (s : SExp) (env : SEnv) (arrenv : SArrEnv) (nenv : NEnv) (narrenv : NArrEnv) : string :=
+  match s with
+    | var_s var => env var
+    | default_s def_ => match def_ with
+                          | error_str => ""
+                          | str sv => sv
+                        end
+    | str_concat s0 s1 => (SEval s0 env arrenv nenv narrenv) ++ (SEval s1 env arrenv nenv narrenv)
+    | arr_s_element arr idx => arrenv arr (AEval idx nenv narrenv)
+  end.
+  
+Fixpoint eval (s : Stmt) (env : Env) (gas : nat) : Env :=
+  match gas with
+    | 0 => env
+    | S gas' => match env with
+      | env_pack nenv benv senv narrenv barrenv sarrenv => match s with
+          | assgn_n var a => env_pack (NUpdate nenv var (AEval a nenv narrenv)) benv senv narrenv barrenv sarrenv
+          | assgn_b var b => env_pack nenv (BUpdate benv var (BEval b benv barrenv nenv narrenv)) senv narrenv barrenv sarrenv
+          | assgn_s var s => env_pack nenv benv (SUpdate senv var (SEval s senv sarrenv nenv narrenv)) narrenv barrenv sarrenv
+          | assgn_arr_n arr idx a => env_pack nenv benv senv (NArrUpdate narrenv arr (AEval idx nenv narrenv) (AEval a nenv narrenv)) barrenv sarrenv
+          | assgn_arr_b arr idx b => env_pack nenv benv senv narrenv (BArrUpdate barrenv arr (AEval idx nenv narrenv) (BEval b benv barrenv nenv narrenv)) sarrenv
+          | assgn_arr_s arr idx s => env_pack nenv benv senv narrenv barrenv (SArrUpdate sarrenv arr (AEval idx nenv narrenv) (SEval s senv sarrenv nenv narrenv))
+          | seq s0 s1 => eval s0 (eval s1 env gas') gas'
+          | while cond s => if (BEval cond benv barrenv nenv narrenv)
+                            then eval (seq s (while cond s)) (env_pack nenv benv senv narrenv barrenv sarrenv) gas'
+                            else env
+          | ifthen b s => if (BEval b benv barrenv nenv narrenv)
+                          then eval s env gas'
+                          else env
+          | ifelse b s0 s1 => if (BEval b benv barrenv nenv narrenv)
+                              then eval s0 env gas'
+                              else eval s1 env gas'
+          | switch_case a0 case_list => match case_list with
+                                        | nil => env
+                                        | c :: rest_of_list => match c with
+                                                      | default_case s => env
+                                                      | case__ a1 s => if (BEval (eq_b a0 a1) benv barrenv nenv narrenv)
+                                                                     then eval s env gas'
+                                                                     else eval (switch_case a0 rest_of_list) env gas'
+                                                      end
+                                       end
+          end
+      end
+  end.
